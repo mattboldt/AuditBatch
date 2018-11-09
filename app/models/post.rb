0d65229d80de0ec714f98c75c956d017558981d7
Matt Boldt <me@mattboldt.com>
2018-11-08 22:32:22 -0600

    progress saving batch audits

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,21 +4,14 @@ class Post < ApplicationRecord
 
   audited associated_with: :user
 
-  def self.update_all_with_audits(updates)
-    result = ActiveRecord::Base.connection.exec_query(select(:id, *updates.keys).to_sql)
-    current_values = result.to_hash
+  scope :foo, -> { where(status: :pending) }
 
-    if update_all(updates)
-      audits = current_values.map do |value|
-        {
-          auditable_id: value['id'],
-          auditable_type: self.name,
-          audited_changes: updates.to_hash,
-          comment: '',
-        }
-      end
-      audits
-      Audited.audit_class.import(audits)
-    end
+  def self.up
+    self.update_all_with_audits(status: :pending)
+  end
+
+  def self.update_all_with_audits(updates)
+    # return unless update_all(updates)
+    AuditBatch.new(self, updates).create
   end
 end
